4d81f9dfe5f7dbdea3a276bf99cece139f2988b3
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-10 16:01:43 -0800

    Create grammar.py

---
+++ grammar/grammar.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from grammarbot import GrammarBotClient
+
+class Grammar(commands.Cog):
+    """Grammar checker."""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener():
+    async def on_message(message):
+        await ctx.send(message)
+
+def setup(bot):
+    bot.add_cog(Grammar(bot))
