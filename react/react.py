c1a2957023553912c1b88a7a009586c92832ae8f
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-15 08:50:55 -0800

    permissions

--- react/react.py
+++ react/react.py
@@ -1,18 +1,23 @@
 import discord
 from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
 
 class ReactOnWord(commands.Cog):
     """Reacts with a banana emoji if someone says a certain word."""
     def __init__(self, bot):
         self.bot = bot
+        self.coll = bot.plugin_db.get_partition(self)
 
     @commands.group()
+    @checks.has_permissions(PermissionLevel.ADMIN)
     async def react(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.send('not good')
 
     @react.command()
     async def word(self, ctx, *, word):
+
         await ctx.send(f'hello and {word}')
 
     @commands.Cog.listener()
