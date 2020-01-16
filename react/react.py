8bd91bc98742b1a39df8b994d9c71caeff034f08
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-16 07:53:36 -0800

    Update react.py

--- react/react.py
+++ react/react.py
@@ -34,7 +34,7 @@ class ReactOnWord(commands.Cog):
                 await message.add_reaction('\N{BANANA}')
         else:
             word = setword["word"]["word"]
-            log.info(type(word))
+            log.info(type(word), - - - - - banana)
             if word.upper in message.content.upper():
                 await message.add_reaction('\N{BANANA}')
 
