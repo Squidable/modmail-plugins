a3e101f9d667d2c93a99c129c9834e7132fc668d
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-16 19:08:49 -0800

    Update react.py

--- react/react.py
+++ react/react.py
@@ -1,8 +1,8 @@
+import logging
 import discord
 from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
-import logging
 
 log = logging.getLogger("Modmail")
 
